29173364da47744f1ec5fe37b1c56adeabbb0795
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-09-26 03:03:00 +0700

    :sparkles: add range potentiometer and ADC with DMC

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -55,6 +55,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void TIM1_UP_TIM10_IRQHandler(void);
 void TIM2_IRQHandler(void);
+void DMA2_Stream0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
